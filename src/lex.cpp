ffc36218043a2e2c09261fbc419e9d899e918ea1
NULL-2333 <450810699@qq.com>
2018-05-02 16:36:49 +0800

    Test 2

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : lex.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
